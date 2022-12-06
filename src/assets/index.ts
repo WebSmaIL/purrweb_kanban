b600967734edcfcfabaf592b97a3ee4cb13f4a9d
ZephWork <smakovskiywebinfo@gmail.com>
2022-12-06 13:40:45 +0600

    fix: delete comments from assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,25 +1,3 @@
-// import accept from "./accept.svg";
-// import comment from "./comment.svg";
-// import pen from "./pen.svg";
-// import plus from "./plus.svg";
-// import userIcon from "./user_icon.svg";
-// import view from "./view.svg";
-// import send from "./send.svg";
-// import close from "./close.svg";
-// import del from './delete.svg'
-
-// const assets = {
-//     accept,
-//     comment,
-//     pen,
-//     plus,
-//     userIcon,
-//     view,
-//     send,
-//     close,
-//     del
-// };
-
 export { default as del } from './delete.svg'
 export { default as accept } from "./accept.svg";
 export { default as comment } from "./comment.svg";
@@ -29,6 +7,3 @@ export { default as userIcon } from "./user_icon.svg";
 export { default as view } from "./view.svg";
 export { default as send } from "./send.svg";
 export { default as close } from "./close.svg";
-
-
-// export default assets;
